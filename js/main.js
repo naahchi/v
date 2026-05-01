9574bbb6829eb49f41dd4756a025dffcc1c09f83
naahchi <botpramote@gmail.com>
2026-05-01 10:25:01 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -129,7 +129,7 @@ function removeActive(items) {
   }
 }
 
-input.addEventListener("keydown", (e) => {
+input?.addEventListener("keydown", (e) => {
   const items = suggestions.getElementsByTagName("li");
 
   if (e.key === "ArrowDown") {
