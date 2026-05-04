cbd3a48d5ad0fdd3dccba41ff126598695e316e8
naahchi <botpramote@gmail.com>
2026-05-04 10:36:11 +0530

    Remove parentheses from category in label

--- js/main.js
+++ js/main.js
@@ -332,7 +332,7 @@ function loadRecentSearches() {
 
     const url = buildUrl(item.state, item.city, item.category);
 
-    const label = `${formatText(item.city)}, ${formatText(item.state)} (${formatText(item.category)})`;
+    const label = `${formatText(item.city)}, ${formatText(item.state)} ${formatText(item.category)}`;
 
     li.innerHTML = `<a href="${url}">${label}</a>`;
     list.appendChild(li);
